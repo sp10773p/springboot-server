8bf493c670a5cf6da5f07158ae548a0a36e54098
dhSeong <sp10773p@gmail.com>
2018-02-06 22:16:33 +0900

    전형적 트랜잭션 처리 빈 추가

--- src/main/java/io/dhseong/core/config/LocalDatabaseConfig.java
+++ src/main/java/io/dhseong/core/config/LocalDatabaseConfig.java
@@ -1,31 +1,23 @@
 package io.dhseong.core.config;
 
-import io.dhseong.Application;
 import net.sf.log4jdbc.Log4jdbcProxyDataSource;
-import net.sf.log4jdbc.SpyLogDelegator;
 import net.sf.log4jdbc.tools.Log4JdbcCustomFormatter;
 import net.sf.log4jdbc.tools.LoggingType;
 import oracle.jdbc.driver.OracleDriver;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
-import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnResource;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.springframework.core.env.Environment;
-import org.springframework.core.io.Resource;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.jdbc.datasource.SimpleDriverDataSource;
-import org.springframework.jndi.JndiObjectFactoryBean;
+import org.springframework.transaction.PlatformTransactionManager;
 
 import javax.sql.DataSource;
-import javax.xml.crypto.Data;
-import java.io.IOException;
-import java.sql.Driver;
 
 /**
  * @author dong hun Seong
@@ -47,16 +39,19 @@ public class LocalDatabaseConfig {
     @Value("${config.datasource.mapperLocation}")
     String mapperLocation;
 
-
-    @Bean(name = "dataSource")
-    public DataSource log4jdbcProxyDataSource(){
+    public DataSource datasourceSpied(){
         SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
         dataSource.setDriverClass(OracleDriver.class);
         dataSource.setUrl("jdbc:oracle:thin:@220.76.203.39:1521:UCS");
         dataSource.setUsername("UCS_FRAME");
         dataSource.setPassword("qazxsw");
 
-        Log4jdbcProxyDataSource jdbcProxyDataSource = new Log4jdbcProxyDataSource(dataSource);
+        return dataSource;
+    }
+
+    @Bean(name = "dataSource")
+    public DataSource log4jdbcProxyDataSource(){
+        Log4jdbcProxyDataSource jdbcProxyDataSource = new Log4jdbcProxyDataSource(datasourceSpied());
         Log4JdbcCustomFormatter log4JdbcCustomFormatter = new Log4JdbcCustomFormatter();
         log4JdbcCustomFormatter.setLoggingType(LoggingType.MULTI_LINE);
         log4JdbcCustomFormatter.setSqlPrefix("SQL       : \n");
@@ -76,4 +71,16 @@ public class LocalDatabaseConfig {
 
         return sqlSessionFactoryBean.getObject();
     }
+
+    /**
+     * 전형적 Transaction
+     */
+    @Bean
+    public PlatformTransactionManager transactionManager(@Qualifier("dataSource") DataSource dataSource){
+        DataSourceTransactionManager transactionManager = new DataSourceTransactionManager();
+        transactionManager.setDataSource(dataSource);
+
+        return transactionManager;
+    }
+
 }
